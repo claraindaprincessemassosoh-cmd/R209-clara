835619e64c3941d4ffb0a78e2fffb66881dbf3e4
claraindaprincessemassosoh-cmd <clarainda.princesse.masso.soh@etu.univ-st-etienne.fr>
2026-05-22 11:32:59 +0200

    Initialisation du projet Kanban

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem', textAlign: 'center' }}>
+      <h1>🚀 Mon premier déploiement</h1>
+      <p>Projet réalisé par <strong>Clarainda Masso Soh</strong></p>
+      <p>Future application : gestion de tâches Kanban</p>
+    </div>
+  );
+}
+
+export default App;
+      
